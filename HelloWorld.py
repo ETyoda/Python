4bf3093b7aeb9935ef2c139b33aa74fb3bbeda3b
Yodahe <etyodahegosa21@gmail.com>
2024-07-01 20:21:43 +0300

    python basic

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+#Hello1 World!
+print("Hello World!")
+#Indentation
+if 5<8:
+    print("Five is less")
+    print("Nothing")
+else:
+    print("Five is more ")
+    
+#variable
+x = 4
+y = 74
+if y>x:
+    print("y" "is grater than" "x")
---
+++ variable.py
@@ -0,0 +1,17 @@
+#variableDecalration
+x,y,z =float(5),"Make",'sum'
+print(x)
+print(y)
+print(z)
+#unpacollection
+name = {"x_sinper","Joe","Mila"}
+print(name)
+x,y,z = name
+print("This's UnpackCollection")
+print(x)
+print(y)
+print(z)
+#outputVariable
+a,b,c,d = 5,'Yoda',43,'jo'
+print(a + c,b + d)
+print(b + d)
