b67eda29a300bf60708c91136ee5efb8ae16a90a
Yodahe <etyodahegosa21@gmail.com>
2024-07-03 22:13:47 +0300

    String

---
+++ formatString.py
@@ -0,0 +1,9 @@
+#f-string
+price=795 
+txt=f"TO buy motor cycle;Purches {price}"
+print(txt)
+#modifier
+dec=f'To buu motor cycle;Purches {price:2f}'
+print(dec)
+#escape Character
+print("Liy join wiz \"jow\".")
