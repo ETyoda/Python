0d00b784ce1f658f9069f07aa81761d1db0bb469
Yodahe <etyodahegosa21@gmail.com>
2024-08-09 23:16:17 +0300

    exercise on inheritance

--- level_9/inheritance.py
+++ level_9/inheritance.py
@@ -7,3 +7,14 @@ class injoy(burgur):
     pass
 piazza_branch = injoy()
 piazza_branch.menu()
+
+# computer model inheritance
+class computer:
+    def __init__(self,brand,model):
+        self.__brand=brand
+        self.__model=model
+    def info(self):
+        return self.__brand + str(self.__model)
+computer_type=computer("acer",2011)
+print(computer_type.info())
+        
